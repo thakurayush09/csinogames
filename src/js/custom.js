97dfa337054b818264c844913a8fe488daf0597e
ayush-thakur <thakurayush511@gmail.com>
2020-01-21 17:28:56 +0530

    baner dine

---
+++ src/js/custom.js
@@ -0,0 +1,14 @@
+
+
+$('.slider_div').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    dots:false,
+    nextArrow:"<img class='a-right control-c next slick-next' src='src/image/previous.png'>",
+    prevArrow:"<img class='a-left control-c prev slick-prev' src='src/image/next.png'>"
+
+
+
+  }
+  );
